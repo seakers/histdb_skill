6815a0225829fa9e115cf5fbace4ad497c7cebd6
Antoni Virós Martin <ani300@gmail.com>
2017-04-04 23:43:36 -0400

    changed skill into history skill

--- __init__.py
+++ __init__.py
@@ -21,43 +21,26 @@ from adapt.intent import IntentBuilder
 from mycroft.skills.core import MycroftSkill
 from mycroft.util.log import getLogger
 
-__author__ = 'eward'
+__author__ = 'antoni'
 
 LOGGER = getLogger(__name__)
 
 
-class HelloWorldSkill(MycroftSkill):
+class HistDBSkill(MycroftSkill):
 
     def __init__(self):
-        super(HelloWorldSkill, self).__init__(name="HelloWorldSkill")
+        super(HistDBSkill, self).__init__(name="HistDBSkill")
 
     def initialize(self):
-        thank_you_intent = IntentBuilder("ThankYouIntent").\
-            require("ThankYouKeyword").build()
-        self.register_intent(thank_you_intent, self.handle_thank_you_intent)
+        history_intent = IntentBuilder("HistoryIntent").require("history").build()
+        self.register_intent(history_intent, self.handle_history_intent)
 
-        how_are_you_intent = IntentBuilder("HowAreYouIntent").\
-            require("HowAreYouKeyword").build()
-        self.register_intent(how_are_you_intent,
-                             self.handle_how_are_you_intent)
-
-        hello_world_intent = IntentBuilder("HelloWorldIntent").\
-            require("HelloWorldKeyword").build()
-        self.register_intent(hello_world_intent,
-                             self.handle_hello_world_intent)
-
-    def handle_thank_you_intent(self, message):
+    def handle_history_intent(self, message):
         self.speak_dialog("welcome")
-
-    def handle_how_are_you_intent(self, message):
-        self.speak_dialog("how.are.you")
-
-    def handle_hello_world_intent(self, message):
-        self.speak_dialog("hello.world")
 
     def stop(self):
         pass
 
 
 def create_skill():
-    return HelloWorldSkill()
+    return HistDBSkill()
